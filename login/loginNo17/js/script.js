2a13307705cd524fa7edaa14eabe0cb2125c5e96
Vladimir Todorovic <42403341+vladRevista@users.noreply.github.com>
2020-01-31 22:07:01 +0100

    fix

--- login/loginNo17/js/script.js
+++ login/loginNo17/js/script.js
@@ -1,6 +1,38 @@
 $(document).ready(function() {
-  $('.flexslider').flexslider({
-    animation: "slide",
-    controlNav: "thumbnails"
-  });
+  
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 1,
+    margin: 0,
+    stagePadding: 0,
+    dots: true,
+    autoplay: false,
+});
+
+dotcount = 1;
+
+jQuery('.owl-dot').each(function () {
+    jQuery(this).addClass('dotnumber' + dotcount);
+    jQuery(this).attr('data-info', dotcount);
+    dotcount = dotcount + 1;
+});
+
+slidecount = 1;
+
+jQuery('.owl-item').not('.cloned').each(function () {
+    jQuery(this).addClass('slidenumber' + slidecount);
+    slidecount = slidecount + 1;
+});
+
+jQuery('.owl-dot').each(function () {
+    grab = jQuery(this).data('info');
+    slidegrab = jQuery('.slidenumber' + grab + ' img').attr('src');
+    jQuery(this).css("background-image", "url(" + slidegrab + ")");
+});
+
+amount = $('.owl-dot').length;
+gotowidth = 100 / amount;
+jQuery('.owl-dot').css("height", gotowidth + "%");
+
+
 });
