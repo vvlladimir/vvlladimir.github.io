fe2bf66b3a181299b1f9f5e3adc168fecc455f8c
Vladimir Todorovic <42403341+vladRevista@users.noreply.github.com>
2020-02-10 16:04:08 +0100

    dashboard

---
+++ speakingPartners/js/script.js
@@ -0,0 +1,11 @@
+
+$("document").ready(function(){
+    
+    // var totalHeight = 0;
+    // $('.card-header [data-toggle="collapse"]').each(function () {
+    //     totalHeight += $(this).outerHeight();
+    //     console.log(totalHeight);
+    //     $(".collapse.show .card-body").css("height", "calc(100vh - " + totalHeight + ")");
+    // });
+
+});
